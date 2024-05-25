b83bb40f41095a8326c061acf056cff8a67e4ec5
Cholyn Sharon Enos <sharoncholyn@gmail.com>
2024-05-25 11:10:27 +0800

    Feat: Add folder Models yang berisi file attribute dari database

---
+++ app/src/main/java/travelreview/project/Models/Model.java
@@ -0,0 +1,13 @@
+package travelreview.project.Models;
+
+public class Model {
+    protected int id;
+
+    public Model(int id) {
+        this.id = id;
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+}
---
+++ app/src/main/java/travelreview/project/Models/User.java
@@ -0,0 +1,36 @@
+package travelreview.project.Models;
+
+public class User extends Model{
+    private String username, name, phoneNumber, email, role;
+    private int age;
+    
+    // Constructors
+    public User(int id, String username, String name, String phoneNumber, String email, String role, int age) {
+        super(id);
+        this.username = username;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.role = role;
+        this.age = age;
+    }
+
+    // Getter & Settter
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getPhoneNumber() {return phoneNumber;}
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getRole() {return role;}
+    public void setRole(String role) {this.role = role;}
+
+    public int getAge() {return age;}
+    public void setAge(int age) {this.age = age;}
+}
