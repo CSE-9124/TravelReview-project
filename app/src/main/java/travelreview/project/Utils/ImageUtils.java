e97a995736c0d3a554f097f04918a236c49b4b9f
Cholyn Sharon Enos <sharoncholyn@gmail.com>
2024-05-29 21:40:45 +0800

    Feat: Create class ImageUtils

---
+++ app/src/main/java/travelreview/project/Utils/ImageUtils.java
@@ -0,0 +1,24 @@
+package travelreview.project.Utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageUtils {
+    public static String saveImage(File imageFile) throws IOException {
+        String targetDirPath = "src/main/resources/images/";
+        File targetDir = new File(targetDirPath);
+
+        File targetFile = new File(targetDir + "/" + imageFile.getName());
+        try (FileInputStream in = new FileInputStream(imageFile);
+            FileOutputStream out = new FileOutputStream(targetFile)) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
+        return targetFile.getPath();
+    }
+}
